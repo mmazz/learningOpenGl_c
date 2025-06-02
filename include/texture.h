1a4a2b2067ecc7dbf6568b70d80fa7dcc7a913ad
mmazz <mazzantimatiass@gmail.com>
2025-06-02 12:54:20 -0300

    Textures

---
+++ include/texture.h
@@ -0,0 +1,11 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include "glad/gl.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+void load_texture(unsigned int* texture, const char* path);
+
+#endif
+
---
+++ src/texture.c
@@ -0,0 +1,38 @@
+#include "texture.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+void load_texture(unsigned int* texture, const char* path)
+{
+    glGenTextures(1, texture);
+    glBindTexture(GL_TEXTURE_2D, *texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);  // IMPORTANTE para PNGs
+    unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        GLenum format = GL_RGB;
+        if (nrChannels == 1)
+            format = GL_RED;
+        else if (nrChannels == 3)
+            format = GL_RGB;
+        else if (nrChannels == 4)
+            format = GL_RGBA;
+
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        printf("Failed to load texture: %s\n", path);
+    }
+    stbi_image_free(data);
+}
+
+
