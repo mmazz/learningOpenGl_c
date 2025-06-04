6aa61a52cb97188a84e488be01a0c3bb1f8f8ace
mmazz <mazzantimatiass@gmail.com>
2025-06-04 15:33:40 -0300

    Refactorizando un poco

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "shader.h"
 #include "texture.h"
 #include "camera.h"
+#include "data.h"
 #include <GLFW/glfw3.h>
 #include <cglm/affine.h> // para funciones como glm_rotate, glm_scale
 #include <cglm/cglm.h>
@@ -11,87 +12,9 @@
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-float vertices2[] = {
-    // positions          // colors           // texture coords
-    0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
-    0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
-    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
-    -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // top left
-};
-float vertices[] = {
-    // Cara trasera
-    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-
-    // Cara delantera
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-
-    // Cara izquierda
-    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f,  1.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 1.0f,
-
-    // Cara derecha
-     0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  0.0f, 1.0f,
-
-    // Cara inferior
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-    // Cara superior
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-};
-unsigned int indices2[] = {
-    // note that we start from 0!
-    0, 1, 3, // first triangle
-    1, 2, 3  // second triangle
-};
-unsigned int indices[] = {
-    // Cara trasera
-    0, 1, 2,  2, 3, 0,
-
-    // Cara delantera
-    4, 5, 6,  6, 7, 4,
-
-    // Cara izquierda
-    8, 9,10, 10,11, 8,
-
-    // Cara derecha
-   12,13,14, 14,15,12,
-
-    // Cara inferior
-   16,17,18, 18,19,16,
-
-    // Cara superior
-   20,21,22, 22,23,20
-};
-
-vec3 cubePositions[] = {
-    {  0.0f,  0.0f,  0.0f },
-    {  2.0f,  5.0f, -15.0f },
-    { -1.5f, -2.2f, -2.5f },
-    { -3.8f, -2.0f, -12.3f },
-    {  2.4f, -0.4f, -3.5f },
-    { -1.7f,  3.0f, -7.5f },
-    {  1.3f, -2.0f, -2.5f },
-    {  1.5f,  2.0f, -2.5f },
-    {  1.5f,  0.2f, -1.5f },
-    { -1.3f,  1.0f, -1.5f }
-};
+extern float vertices[];
+extern unsigned int indices[];
+extern vec3 cubePositions[];
 
 float deltaTime = 0.f;
 float lastFrame = 0.f;
@@ -101,13 +24,72 @@ vec3 cameraPos   = {0.0f, 0.0f, 3.0f};
 vec3 cameraFront = {0.0f, 0.0f, -1.0f};
 vec3 cameraUp    = {0.0f, 1.0f,  0.0f};
 
-
-
 // Campo de visión
 static float lastX = 800.0f / 2.0f;
 static float lastY = 600.0f / 2.0f;
 static bool firstMouse = true;
 static bool constrainPitch = false;
+
+
+void processInput(GLFWwindow* window, float deltaTime);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+bool init_glad();
+void init_buffers(unsigned int *VAO, unsigned int *VBO, unsigned int *EBO, float *vertices, size_t vertices_size, unsigned int *indices, size_t indices_size);
+void init_texture(GLuint shaderProgram, GLuint *tex, const char *path, const char *uniformName, int textureUnit);
+void render_scene(GLuint shaderProgram, Camera *camera, GLuint VAO, GLuint EBO);
+GLuint init_shader_program(const char *vertexPath, const char *fragmentPath);
+GLFWwindow *setup_window_and_camera(int width, int height, const char* title, Camera** out_camera);
+
+int main() {
+    Camera* camera = NULL;
+    GLFWwindow* window = setup_window_and_camera(SCR_WIDTH, SCR_HEIGHT, "Simulator", &camera);
+    if (!window) {
+        printf("No windows created");
+        return -1;
+    }
+    if (!init_glad()) {
+        printf("Error at glad init");
+        return -1;
+    }
+    glEnable(GL_DEPTH_TEST);
+    GLuint VAO, VBO, EBO;
+    init_buffers(&VAO, &VBO, &EBO, vertices, sizeof(vertices), indices, sizeof(indices));
+
+    GLuint shaderProgram = init_shader_program("src/shaders/vertex.glsl", "src/shaders/fragment.glsl");
+    GLuint texture1, texture2;
+    init_texture(shaderProgram, &texture1, "assets/wall.jpg", "texture1", 0);
+    init_texture(shaderProgram, &texture2, "assets/awesomeface.png", "texture2", 1);
+
+    while (!glfwWindowShouldClose(window)) {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        processInput(window, deltaTime);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        render_scene(shaderProgram, camera, VAO, EBO);
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+    glfwTerminate();
+    return 0;
+}
 
 void processInput(GLFWwindow* window, float deltaTime) {
     Camera* camera = (Camera*)glfwGetWindowUserPointer(window);
@@ -152,14 +134,36 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
   glViewport(0, 0, width, height);
 }
 
-int init_glad(){
-  if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
-    printf("Failed to initialize GLAD");
-    return -1;
-  }
-    return 0;
+void init_buffers(unsigned int *VAO, unsigned int *VBO, unsigned int *EBO, float *vertices, size_t vertices_size, unsigned int *indices, size_t indices_size) {
+    glGenVertexArrays(1, VAO);
+    glGenBuffers(1, VBO);
+    glGenBuffers(1, EBO);
+
+    glBindVertexArray(*VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, *VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices_size, vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices_size, indices, GL_STATIC_DRAW);
+
+    // Asume que el layout es: vec3 posición + vec2 texcoords
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
 }
-GLFWwindow *init_window(int width, int height, const char* title){
+
+bool init_glad() {
+    if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
+        fprintf(stderr, "Failed to initialize GLAD\n");
+        return false;
+    }
+    return true;
+}
+GLFWwindow *setup_window_and_camera(int width, int height, const char* title, Camera** out_camera){
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -171,123 +175,70 @@ GLFWwindow *init_window(int width, int height, const char* title){
     return NULL;
   }
   glfwMakeContextCurrent(window);
-    return window;
-}
-int main() {
-  GLFWwindow* window = init_window(SCR_WIDTH, SCR_HEIGHT, "Simulator");
+  glfwSwapInterval(1); //Usá V-Sync para que GLFW sincronice el swap de buffers con la frecuencia de actualización del monitor
   Camera* camera = camera_init(cameraPos, cameraUp, YAW, PITCH);
+    if (!camera) {
+        glfwDestroyWindow(window);
+        return NULL;
+    }
   glfwSetWindowUserPointer(window, camera);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
   glfwSetCursorPosCallback(window, mouse_callback);
   glfwSetScrollCallback(window, scroll_callback);
   glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-  init_glad();
-
-  unsigned int VBO, VAO, EBO;
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
-  glGenBuffers(1, &EBO);
-
-  glBindVertexArray(VAO);
-
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-               GL_STATIC_DRAW);
+  *out_camera = camera;
+  return window;
+}
 
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-  glBindVertexArray(0);
-
-
-  const char *vertexFilePath = "src/shaders/vertex.glsl";
-  unsigned int vertexShaderID;
-  compile_shader(&vertexShaderID, GL_VERTEX_SHADER, vertexFilePath);
-
-  const char *fragmentFilePath = "src/shaders/fragment.glsl";
-  unsigned int fragmentShaderID;
-  compile_shader(&fragmentShaderID, GL_FRAGMENT_SHADER, fragmentFilePath);
-
-  GLuint shaderProgram = link_shader(vertexShaderID, fragmentShaderID);
-
-  unsigned int texture1;
-  const char *texture_file = "assets/wall.jpg";
-  load_texture(&texture1, texture_file);
-  unsigned int texture2;
-  texture_file = "assets/awesomeface.png";
-  load_texture(&texture2, texture_file);
-
-  glUseProgram(shaderProgram);
-  glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0);
-  glUniform1i(glGetUniformLocation(shaderProgram, "texture2"), 1);
+GLuint init_shader_program(const char *vertexPath, const char *fragmentPath) {
+    GLuint vertexShader, fragmentShader;
+    compile_shader(&vertexShader, GL_VERTEX_SHADER, vertexPath);
+    compile_shader(&fragmentShader, GL_FRAGMENT_SHADER, fragmentPath);
+    return link_shader(vertexShader, fragmentShader);
+}
 
 
-    //habilito el z-depth
-  glEnable(GL_DEPTH_TEST);
-  while (!glfwWindowShouldClose(window)) {
-    float currentFrame = glfwGetTime();
-    deltaTime = currentFrame - lastFrame;
-    lastFrame = currentFrame;
-    // -----
-    processInput(window, deltaTime);
 
-    // render
-    // ------
+void init_texture(GLuint shaderProgram, GLuint *tex, const char *path, const char *uniformName, int textureUnit) {
+    load_texture(tex, path);  // Esta debe bindear y configurar GL_TEXTURE_2D, típicamente a GL_TEXTURE0 + textureUnit
+
+    glUseProgram(shaderProgram);
+    GLint location = glGetUniformLocation(shaderProgram, uniformName);
+    if (location == -1) {
+        fprintf(stderr, "Warning: uniform '%s' not found in shader\n", uniformName);
+    } else {
+        glUniform1i(location, textureUnit);
+    }
+}
+
+void render_scene(GLuint shaderProgram, Camera *camera, GLuint VAO, GLuint EBO) {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, texture1);
-    glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_2D, texture2);
-
     glUseProgram(shaderProgram);
 
-
+    // Proyección y vista
     mat4 projection;
-    glm_perspective(glm_rad(camera->Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f, projection);
-
-    unsigned int projLoc = glGetUniformLocation(shaderProgram, "projection");
-    glUniformMatrix4fv(projLoc, 1, GL_FALSE, (float *)projection);
+    glm_perspective(glm_rad(camera->Zoom), (float)SCR_WIDTH / SCR_HEIGHT, 0.1f, 100.0f, projection);
+    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, (float *)projection);
 
     mat4 view;
     camera_get_view_matrix(camera, view);
+    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, (float *)view);
 
-    unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
-    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, (float *)view);
-
+    // Modelado y dibujado
     glBindVertexArray(VAO);
-    for(unsigned int i = 0; i < 10; i++)
-    {
+    for (unsigned int i = 0; i < 10; i++) {
         mat4 model;
         glm_mat4_identity(model);
-
         glm_translate(model, cubePositions[i]);
+
         float angle = 20.0f * i;
-        glm_rotate(model, (float)glfwGetTime() *glm_rad(angle), (vec3){1.0f, 0.3f, 0.5f});
-        unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, (float *)model);
+        glm_rotate(model, glfwGetTime() * glm_rad(angle), (vec3){1.0f, 0.3f, 0.5f});
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, (float *)model);
 
         glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
     }
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-
-
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
-  glDeleteVertexArrays(1, &VAO);
-  glDeleteBuffers(1, &VBO);
-  glDeleteBuffers(1, &EBO);
-  glDeleteProgram(shaderProgram);
-  glfwTerminate();
-  return 0;
+    glBindVertexArray(0);
 }
-
